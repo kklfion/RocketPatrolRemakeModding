9a37b4693296426d048cdedad7f22b5313180ecf
Fion Kwok <fikwok@ucsc.edu>
2021-04-17 21:22:16 -0700

    Recovered the prefab from the original version.

---
+++ src/prefabs/BonusTarget.js
@@ -0,0 +1,23 @@
+// BonusTarget prefab
+class BonusTarget extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        this.points = pointValue;
+        this.movementSpeed = game.settings.spaceshipSpeed * 2;
+    }
+  
+    update() {
+        this.x -= this.movementSpeed;
+  
+        if(this.x <= 0 - this.width) {
+            this.reset();
+        }
+    }
+  
+    //resets the position of the ship
+    reset() {
+        this.x = game.config.width;
+    }
+  }
+  
